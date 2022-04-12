f299c856e0d5a8b7c9857f3f4a4cc0cb727c4afb
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-12 15:23:20 -0400

    dbupdater

--- dbUpdater/views.py
+++ dbUpdater/views.py
@@ -3,5 +3,21 @@ from schoolFinder import models
 from django.shortcuts import render
 
 # Create your views here.
+from schoolFinder.models import MajorDegree, School
+
+
 def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def updateDB(request):
+    a = MajorDegree(major="test school 123",
+                    school=School(institution_name="test",
+                                  number_applicants="12",
+                                  percent_applicants_admitted="123",
+                                  act_75="0",
+                                  act_25="0",
+                                  graduation_rate="0.023"))
+    a.save()
+
     return HttpResponse("Hello, world. You're at the polls index.")
