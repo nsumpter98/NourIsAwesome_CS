f0edd30d7a36b2d7a6a04d2a8a9bb03c9cec2c1f
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-14 17:21:11 -0400

    misc

--- dbUpdater/views.py
+++ dbUpdater/views.py
@@ -1,16 +1,6 @@
 from django.http import HttpResponse
 from schoolFinder import models
 from django.shortcuts import render
-import pandas as pd
-import requests
-import io
-
-url = "https://raw.githubusercontent.com/nsumpter98/NourIsAwesome_CS/master/CSVFiles/schools.csv"
-download = requests.get(url).content
-
-df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-
-
 
 # Create your views here.
 from schoolFinder.models import MajorDegree, School
@@ -28,7 +18,14 @@ def index(request):
     a = MajorDegree(major="test school 123",
                     school=sc)
     a.save()
-    return HttpResponse(download)
+    e = MajorDegree.objects.filter(major="CS")
+
+    for result in e:
+        print(result.major)
+        print(result.school)
+    return HttpResponse(e)
+
+
 
 
 
