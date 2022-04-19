9a206ec9a9609d6fddcae034733511ff4c00b929
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-19 15:27:14 -0400

    nathan

--- schoolFinder/models.py
+++ schoolFinder/models.py
@@ -10,7 +10,7 @@ class School(models.Model):  # Id: 1, 2, 3
     graduation_rate = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.institution_name
+        return self.institution_name#test
 
 
 class Degree(models.Model):
